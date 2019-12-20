3dae151560a8ae018f63592d18613c7ec00e4203
forest-k <kzuki.mori@gmail.com>
2019-12-20 15:41:17 +0900

    update cors configuration

--- src/main/kotlin/jp/co/musako/config/WebSecurityConfig.kt
+++ src/main/kotlin/jp/co/musako/config/WebSecurityConfig.kt
@@ -1,24 +1,17 @@
 package jp.co.musako.config
 
-import jp.co.musako.application.authentication.CustomAccessDeniedHandler
-import jp.co.musako.application.authentication.CustomAuthenticationEntryPoint
-import jp.co.musako.application.authentication.CustomAuthenticationFailureHandler
-import jp.co.musako.application.authentication.CustomAuthenticationSuccessHandler
-import jp.co.musako.application.filter.CustomUsernamePasswordAuthenticationFilter
-import jp.co.musako.domain.service.CustomUserDetailsService
-import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.savedrequest.NullRequestCache
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import jp.co.musako.application.authentication.*
+import jp.co.musako.application.filter.*
+import jp.co.musako.domain.service.*
+import org.slf4j.*
+import org.springframework.context.annotation.*
+import org.springframework.security.config.annotation.authentication.builders.*
+import org.springframework.security.config.annotation.web.builders.*
+import org.springframework.security.config.annotation.web.configuration.*
+import org.springframework.security.crypto.password.*
+import org.springframework.security.web.authentication.*
+import org.springframework.security.web.savedrequest.*
+import org.springframework.web.cors.*
 
 @Configuration
 @EnableWebSecurity
@@ -104,10 +97,11 @@ class WebSecurityConfig(
 
     private fun customCorsConfigurationSource(): CorsConfigurationSource {
         val corsConfiguration = CorsConfiguration()
-        corsConfiguration.addAllowedMethod("*")
-        corsConfiguration.addAllowedOrigin("*")
-        corsConfiguration.addAllowedHeader("X-AUTH-KEY")
-        corsConfiguration.setMaxAge(1800L)
+        corsConfiguration.allowedMethods = listOf(CorsConfiguration.ALL)
+        corsConfiguration.allowedOrigins = listOf(CorsConfiguration.ALL)
+        corsConfiguration.allowedHeaders = listOf(CorsConfiguration.ALL)
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.maxAge = 1800L
 
         val corsSource = UrlBasedCorsConfigurationSource()
         corsSource.registerCorsConfiguration("/*", corsConfiguration)
